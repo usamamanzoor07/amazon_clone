149a95bfc7457645beb3a09743f12b991e9a0b90
MuhammadUsama07 <muhammad.usama@wgroup.tech>
2022-06-28 02:06:50 +0500

    User Model Added

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 // IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
 
 // IMPORTS FROM OTHER FILES
 const authRouter = require("./routes/auth");
 
-
 // INITIALIZE
 const PORT = 3000;
 const app = express();
+const DB =
+  "mongodb+srv://Muhammad_Usama:FlutterAmazonClone@cluster0.3ouoi.mongodb.net/?retryWrites=true&w=majority";
 
 //MIDDLEWARE
 app.use(authRouter);
+
+//CONNECTIONS
+mongoose.connect(DB).then(() => {
+  console.log("Connection Successful");
+});
 
 app.listen(PORT, () => {
   console.log(`connected at port ${PORT}`);
